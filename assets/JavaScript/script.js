1191b8f47416e94011817eace962495887442894
Douglas Lima <douglas07a@gmail.com>
2023-03-07 09:53:21 -0300

    Troca do arquivo JS, e atualização do mesmo

---
+++ assets/JavaScript/script.js
@@ -0,0 +1,252 @@
+//Botão para as notas e acorde de Sol //
+const button1 = document.querySelector('#Solmaior')
+const button2 = document.querySelector('#Laa')
+const button3 = document.querySelector('#Sii')
+const button4 = document.querySelector('#Doo')
+const button5 = document.querySelector('#Ree')
+const button6 = document.querySelector('#Mii')
+const button7 = document.querySelector('#Faa')
+const button8 = document.querySelector('#Solmenor')
+const button9 = document.querySelector('#escalasol')
+//-------------------------------------------------//
+
+
+//Botão para as notas e acorde de Ré //
+const button10 = document.querySelector('#Rmaior')
+const button11 = document.querySelector('#remi')
+const button12 = document.querySelector('#refa')
+const button13 = document.querySelector('#resol')
+const button14 = document.querySelector('#rela')
+const button15 = document.querySelector('#resi')
+const button16 = document.querySelector('#redo')
+const button17 = document.querySelector('#Rmenor')
+const button18 = document.querySelector('#escaladere')
+//-------------------------------------------------//
+
+
+//Botão para as notas e acorde de Mi //
+const button19 = document.querySelector('#Mmaior')
+const button20= document.querySelector('#mifa')
+const button21 = document.querySelector('#misol')
+const button22= document.querySelector('#mila')
+const button23 = document.querySelector('#misi')
+const button24 = document.querySelector('#mido')
+const button25 = document.querySelector('#mire')
+const button26 = document.querySelector('#mmenor')
+const button27 = document.querySelector('#acordemi')
+//-------------------------------------------------//
+
+
+//Botão para as notas e acorde de Dó //
+const button28 = document.querySelector('#Dmaior')
+const button29= document.querySelector('#dore')
+const button30 = document.querySelector('#domi')
+const button31 = document.querySelector('#dofa')
+const button32 = document.querySelector('#dosol')
+const button33 = document.querySelector('#dola')
+const button34 = document.querySelector('#dosi')
+const button35 = document.querySelector('#dmenor')
+const button36 = document.querySelector('#acordedo')
+//-------------------------------------------------//
+
+
+
+
+//Audio para as notas e acorde de Sol //
+button1.addEventListener('click', function(){
+    const audio = document.querySelector('#acordesSol')
+    audio.play()
+ })
+
+ button2.addEventListener('click', function(){
+    const audio = document.querySelector('#acordesLa')
+    audio.play()
+ })
+
+ button3.addEventListener('click', function(){
+   const audio = document.querySelector('#acordesSi')
+   audio.play()
+})
+
+button4.addEventListener('click', function(){
+   const audio = document.querySelector('#acordesDo')
+   audio.play()
+})
+
+button5.addEventListener('click', function(){
+   const audio = document.querySelector('#acordesRe')
+   audio.play()
+})
+
+button6.addEventListener('click', function(){
+   const audio = document.querySelector('#acordesMi')
+   audio.play()
+})
+
+button7.addEventListener('click', function(){
+   const audio = document.querySelector('#acordesFa')
+   audio.play()
+})
+
+button8.addEventListener('click', function(){
+   const audio = document.querySelector('#acordesSolm')
+   audio.play()
+})
+
+button9.addEventListener('click', function(){
+   const audio = document.querySelector('#acordedesol')
+   audio.play()
+})
+//-------------------------------------------------//
+
+
+
+
+//Audio para as notas e acorde de Re //
+button10.addEventListener('click', function(){
+    const audio = document.querySelector('#acorderRe')
+    audio.play()
+ })
+
+button11.addEventListener('click', function(){
+    const audio = document.querySelector('#acorderMi')
+    audio.play()
+ })
+
+button12.addEventListener('click', function(){
+   const audio = document.querySelector('#acorderFa')
+   audio.play()
+})
+
+button13.addEventListener('click', function(){
+   const audio = document.querySelector('#acordersol')
+   audio.play()
+})
+
+button14.addEventListener('click', function(){
+   const audio = document.querySelector('#acorderla')
+   audio.play()
+})
+
+button15.addEventListener('click', function(){
+   const audio = document.querySelector('#acordersi')
+   audio.play()
+})
+
+button16.addEventListener('click', function(){
+   const audio = document.querySelector('#acorderdo')
+   audio.play()
+})
+
+button17.addEventListener('click', function(){
+   const audio = document.querySelector('#acorderRem')
+   audio.play()
+})
+
+button18.addEventListener('click', function(){
+   const audio = document.querySelector('#acordedeRe')
+   audio.play()
+})
+//-------------------------------------------------//
+
+
+
+
+//Audio para as notas e acorde de Mi //
+button19.addEventListener('click', function(){
+    const audio = document.querySelector('#acordemmi')
+    audio.play()
+ })
+
+ button20.addEventListener('click', function(){
+    const audio = document.querySelector('#acordemfa')
+    audio.play()
+ })
+
+ button21.addEventListener('click', function(){
+   const audio = document.querySelector('#acordemsol')
+   audio.play()
+})
+
+button22.addEventListener('click', function(){
+   const audio = document.querySelector('#acordemla')
+   audio.play()
+})
+
+button23.addEventListener('click', function(){
+   const audio = document.querySelector('#acordemsi')
+   audio.play()
+})
+
+button24.addEventListener('click', function(){
+   const audio = document.querySelector('#acordemdo')
+   audio.play()
+})
+
+button25.addEventListener('click', function(){
+   const audio = document.querySelector('#acordemre')
+   audio.play()
+})
+
+button26.addEventListener('click', function(){
+   const audio = document.querySelector('#acordemmim')
+   audio.play()
+})
+
+button27.addEventListener('click', function(){
+   const audio = document.querySelector('#escalademi')
+   audio.play()
+})
+//-------------------------------------------------//
+
+
+
+
+//Audio para as notas e acorde de Do //
+button28.addEventListener('click', function(){
+    const audio = document.querySelector('#acordeddo')
+    audio.play()
+ })
+
+ button29.addEventListener('click', function(){
+    const audio = document.querySelector('#acordedre')
+    audio.play()
+ })
+
+ button30.addEventListener('click', function(){
+   const audio = document.querySelector('#acordedmi')
+   audio.play()
+})
+
+button31.addEventListener('click', function(){
+   const audio = document.querySelector('#acordedfa')
+   audio.play()
+})
+
+button32.addEventListener('click', function(){
+   const audio = document.querySelector('#acordedsol')
+   audio.play()
+})
+
+button33.addEventListener('click', function(){
+   const audio = document.querySelector('#acordedla')
+   audio.play()
+})
+
+button34.addEventListener('click', function(){
+   const audio = document.querySelector('#acordedsi')
+   audio.play()
+})
+
+button35.addEventListener('click', function(){
+   const audio = document.querySelector('#acordeddom')
+   audio.play()
+})
+
+button36.addEventListener('click', function(){
+   const audio = document.querySelector('#escaladedo')
+   audio.play()
+})
+//-------------------------------------------------//
+
+
